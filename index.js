d13d6f7c1c6fccd6944fffd3bf3bdb14a1334b72
Your Name~ <you@example.com>
2023-12-07 10:16:08 +0600

    vercel config file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port =process.env.PORT || 5000
 
 
 const categories = require('./Data/categories.json')
+const news =require('./Data/news.json')
 
 app.use(cors())
 
@@ -15,6 +16,28 @@ res.send('world news is comming')
 
 app.get('/categories',(req,res)=>{
     res.send(categories)
+})
+
+app.get('/news', (req,res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req,res)=>{
+    const id= req.params.id
+    console.log(id)
+    const selectedNews =news.find(n=> n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/category/:id',(req,res)=>{
+    const id =parseInt(req.params.id)
+    console.log(id)
+    if(id ===0){
+        res.send(news)
+    }else{
+        const categoryNews =news.filter(n=> parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, () => {
